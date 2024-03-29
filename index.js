62dfe00c12831ef5e72bcdaa5cdd580deb814100
joshua mwendwa <leeroyjoshu087@gmail.com>
2024-03-29 17:54:01 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+    input.addEventListener('click', handleClick);
+  }
+  
+  function handleClick() {
+    console.log('Button clicked');
+  }
